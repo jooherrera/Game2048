95b70a89a192a2a9f3d5a2b17e49808c5bbe91c4
Jose Herrera <jooherrera4@gmail.com>
2024-03-28 23:17:30 -0300

    add game class interface

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,5 +1,16 @@
 package game;
 
+import java.util.List;
+
 public interface Game {
 
+	void moveToRight();
+	void moveToLeft();
+	void moveToUp();
+	void moveToDwn();
+	
+	int getScore();
+	List<List<Integer>> getBoard();
+	boolean hasPlayerWon();
+	boolean hasPlayerLose();
 }
