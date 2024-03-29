972b8b662dd3f4760a0a3df0bc41890a3726b1bf
Jose Herrera <jooherrera4@gmail.com>
2024-03-28 21:30:56 -0300

    feat: Score class (#1)

--- src/game/Score.java
+++ src/game/Score.java
@@ -2,4 +2,22 @@ package game;
 
 public class Score {
 
+	private int value;
+	
+	public Score() {
+		this.value = 0;
+	}
+	
+	public void add(int value) {
+		if(value < 0) {
+			throw new IllegalArgumentException("No se puede agregar al score un valor negativo: " + value);
+		}
+		
+		this.value += value;
+		
+	}
+	
+	public int getValue() {
+		return this.value;
+	}
 }
