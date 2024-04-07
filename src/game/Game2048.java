57e6ce87c5a58a1a7c9dd37f3d9adbff5d220b96
Jose Herrera <jooherrera4@gmail.com>
2024-04-06 22:32:41 -0300

    implements game

--- src/game/Game2048.java
+++ src/game/Game2048.java
@@ -1,5 +1,53 @@
 package game;
 
-public class Game2048 {
+import java.util.List;
+
+public class Game2048 implements Game {
+	
+	Board board = new Board();
+
+	@Override
+	public void moveToRight() {
+		board.combineToRight();
+	}
+
+	@Override
+	public void moveToLeft() {
+		board.combineToLeft();
+		
+	}
+
+	@Override
+	public void moveToUp() {
+		board.combineToUp();
+	}
+
+	@Override
+	public void moveToDwn() {
+		board.combineToDown();	
+	}
+
+	@Override
+	public int getScore() {
+		return board.getScore();
+	}
+
+	@Override
+	public List<List<Integer>> getBoard() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean hasPlayerWon() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean hasPlayerLose() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
